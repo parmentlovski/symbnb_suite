ded3aed2d1f6dac05f43101b994b15576e88bae6
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-03-04 15:01:32 +0000

    Ajout du bundle leage/csv pour exporter données en fichier csv

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Egyg33k\CsvBundle\Egyg33kCsvBundle::class => ['all' => true],
 ];
