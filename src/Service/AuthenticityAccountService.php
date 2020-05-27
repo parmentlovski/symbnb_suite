20a67e7b11c20b9d665056311a43a5c39024d7f0
saitama93 <igalilmi32@gmail.com>
2020-05-27 04:41:08 +0000

    Mise en place de l récupération du mot de passe oublié

---
+++ src/Service/AuthenticityAccountService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\UserRepository;
+
+class AuthenticityAccountService
+{
+
+    public function __construct(UserRepository $userRepo)
+    {
+        $this->userRepo = $userRepo;
+    }
+
+    /**
+     * Permet de vérifier si l'adresse mail donnée correspond à celle d'un compte existant
+     *
+     * @param string $email
+     * @return boolean
+     */
+    public function checkEmail(string $email)
+    {
+        foreach ($this->userRepo->findAll() as $user) {
+            if ($user->getEmail() == $email) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
