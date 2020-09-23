32ebeb012167bd4bf15fec9d2674cd6f99eae20c
saitama93 <igalilmi32@gmail.com>
2020-09-23 18:10:40 +0200

    Correction des bug lié aux fichiers  de migrations

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,7 +11,7 @@ use App\Entity\Booking;
 use App\Entity\Comment;
 use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
