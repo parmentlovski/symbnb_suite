1e766d238fccd74369d856548cd3746a1e353590
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-10-01 13:55:21 +0000

    Mise en commentaire function up de la migration empechant lancement heroku

--- src/Migrations/Version20200930150327.php
+++ src/Migrations/Version20200930150327.php
@@ -20,7 +20,7 @@ final class Version20200930150327 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE migration_versions');
+        // $this->addSql('DROP TABLE migration_versions');
         // $this->addSql('ALTER TABLE ad DROP cover_image');
         // $this->addSql('ALTER TABLE booking ADD payment TINYINT(1) NOT NULL');
     }
